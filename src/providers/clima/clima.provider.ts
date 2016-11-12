ffe5cbdc21139115145179ba36c6980eb8ad72ba
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-11-12 08:52:57 -0500

    clima provider implemented

--- src/providers/clima/clima.provider.ts
+++ src/providers/clima/clima.provider.ts
@@ -1,4 +1,24 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "../httpclient/httpclient.provider"
+import { Observable } from "rxjs/observable";
+import { Response } from "@angular/http"
 
 @Injectable()
-export class ClimaProvider {}
+export class ClimaProvider extends HttpClient {
+
+    url:string = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22popayan%2C%20co%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+    loadWeather():Observable<any>{
+        return this.get(this.url).map(this.processResponse).catch(this.processError);
+    }
+
+    private processResponse(res:Response){
+
+    }
+
+    private processError(){
+        return Observable.throw(true);
+    }
+
+
+}
